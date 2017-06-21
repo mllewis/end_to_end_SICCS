a8c715f5deaa00c865763a8665bdab7fc9e545dc
myeomans <mk.yeomans@gmail.com>
2017-06-21 15:37:50 -0400

    MI-SLOR wordplot

--- wordplot.R
+++ wordplot.R
@@ -1,13 +1,40 @@
-course.name<-"Negotiation Bots"
+# wordPlot(dataparse$allcleantext,
+#          dataparse$success,
+#          "Negotiation Bots")
 
-# Convert representations
-textMatrix <- quanteda::dfm(dataparse$cleantext, stem=TRUE, remove=stopwords(),
-                           removePunct=TRUE,removeSymbols=TRUE, removeNumbers=TRUE)
+wordPlot<-function(text,outcome,title="Insert Title"){
+  
+  # Convert representations
+  textMatrix <- quanteda::dfm(text, stem=TRUE, remove=stopwords(),
+                              removePunct=TRUE,removeSymbols=TRUE, removeNumbers=TRUE)
+  
+  textRating <- scale(outcome)
+  
+  text.results <- cmp.slor.mis(textMatrix, textRating)
+  # Display mutual information plot
+  grayline<- quantile(text.results$mis,.5)
+  
+  ggplot(data=text.results) +
+    #geom_point(aes(x=slor, y=mis, color=slor), size=0) + 
+    geom_text(aes(x=slor, y=mis, label=colnames(textMatrix)),
+              alpha=ifelse(text.results$mis > grayline, 1, text.results$mis * (150)*(0.005/grayline)),
+              color=ifelse(text.results$col, "firebrick", "dodgerblue"),
+              size=4,
+              nudge_y=0.00) +
+    guides(fill=FALSE) +
+    # scale_color_gradient2(low="dodgerblue", mid="gray97", high="firebrick", midpoint=0,
+    #                       breaks=c(min(text.results$slor)+0.5, max(text.results$slor)-0.5),
+    #                       labels=c("More liberal","More conservative")) + 
+    theme_bw() + 
+    labs(title=title,
+         x="Variance-weighted log-odds ratio",
+         y="Mutual Information",
+         color="Partisanship") +
+    theme(plot.title = element_text(hjust = 0.5, size=20, family="Times",face="bold"),
+          axis.title = element_text(family="Times",face="bold", size=20), legend.position="none")
+}
 
-textRating <- scale(dataparse$success)
-
-
-#textMatrix<-DTMtools::DTM(text.data$body, ngrams=1:3,sparse=0.99)
+#textMatrix<-DTMtools::DTM(text.data$body, ngrams=1:3,sparse=0.99, stop.words=F)
 
 # Function to compute (variance-weighted) log-odds
 #  x is a V-length count vector for the numerator group
@@ -47,25 +74,3 @@ cmp.slor.mis <- function(textMatrix, rating) {
   results <- data.frame(slor =  slor, mis = mis) %>% mutate(col = slor > 0)
   return(results)
 }
-text.results <- cmp.slor.mis(textMatrix, textRating)
-# Display mutual information plot
-grayline<- quantile(text.results$mis,.5)
-
-ggplot(data=text.results) +
-  #geom_point(aes(x=slor, y=mis, color=slor), size=0) + 
-  geom_text(aes(x=slor, y=mis, label=colnames(textMatrix)),
-            alpha=ifelse(text.results$mis > grayline, 1, text.results$mis * (150)*(0.005/grayline)),
-            color=ifelse(text.results$col, "firebrick", "dodgerblue"),
-            size=4,
-            nudge_y=0.00) +
-  guides(fill=FALSE) +
-  # scale_color_gradient2(low="dodgerblue", mid="gray97", high="firebrick", midpoint=0,
-  #                       breaks=c(min(text.results$slor)+0.5, max(text.results$slor)-0.5),
-  #                       labels=c("More liberal","More conservative")) + 
-  theme_bw() + 
-  labs(title=course.name,
-       x="Variance-weighted log-odds ratio",
-       y="Mutual Information",
-       color="Partisanship") +
-  theme(plot.title = element_text(hjust = 0.5, size=20, family="Times",face="bold"),
-        axis.title = element_text(family="Times",face="bold", size=20), legend.position="none")
